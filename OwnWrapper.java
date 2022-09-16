6c8a324c12760b60daee58b9c6dde292be12d69d
ganesh2468 <18btris018@jainuniversity.ac.in>
2022-09-16 17:48:42 +0530

    Ownwrapper

--- OwnWrapper.java
+++ OwnWrapper.java
@@ -1,21 +1,11 @@
-import java.sql.SQLOutput;
-
 public class OwnWrapper {
     int i;
     OwnWrapper(int i){
         this.i=i;
     }
-
-    public String toBinary(int i) {
-        System.out.println("binary");
-        return Integer.toBinaryString(i);
-    }
     public String toString(){
-        System.out.println("string");
         return Integer.toString(i);
     }
-
-//Testing the custom wrapper class
     public static void main(String[] args){
         OwnWrapper j=new OwnWrapper(10);
         System.out.println(j);
